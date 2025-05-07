12b4643476c5bbfb51ee8f3468c58aae7088e0ba
e-chan1007 <echan041007@gmail.com>
2025-05-07 14:18:10 +0900

    Update run_once_completions.sh

--- run_once_completions.sh
+++ run_once_completions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p "${ZDOTDIR}/completions"
+mkdir -p "${ZDOTDIR:-~/.config/zsh}/completions"
 
 if command -v proto >/dev/null 2>&1; then
   proto completions --shell zsh > "${ZDOTDIR}/completions/_proto"
